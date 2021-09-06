715d48afeb8576c98abf4507f7c8dd5defcf1fab
Ad019El <a.dafi@esi-sba.dz>
2021-09-06 03:24:45 +0100

    fix server

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-const express = require('express');
-const connectDB = require('./config/db');
-const path = require('path');
-=======
 const express = require("express");
 const connectDB = require("./config/db");
 const path = require("path");
->>>>>>> 8e07dc82 (added programme)
 
 const app = express();
 
@@ -19,19 +13,6 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // Routes
-<<<<<<< HEAD
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/request', require('./routes/request'));
-
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-=======
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/request", require("./routes/request"));
@@ -43,7 +24,6 @@ if (process.env.NODE_ENV === "production") {
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
->>>>>>> 8e07dc82 (added programme)
   );
 }
 

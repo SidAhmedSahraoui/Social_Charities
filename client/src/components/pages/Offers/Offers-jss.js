0f3b34fe6e3e1d242f227d85e5c1d59b881294d3
Ad019El <a.dafi@esi-sba.dz>
2021-09-06 02:35:52 +0100

    initial commit

--- client/src/components/pages/Offers/Offers-jss.js
+++ client/src/components/pages/Offers/Offers-jss.js
@@ -1,72 +1,69 @@
 import { createUseStyles } from "react-jss";
-import colors from '../../../Styles/Color'
-
+import colors from "../../../Styles/Color";
 
 const useStyles = createUseStyles({
   root: {
-    padding: '40px 20px',
-    marginBottom: '40px',
-    '&.card-shadow':{
-      borderRadius: '10px',
-      'box-shadow': '0px 13px 26px rgb(109,184,241, 0.16)',   
-      
-      '& .img' : {
-        maxWidth : '50px',
-        maxHeight : '50px'
+    padding: "40px 20px",
+    marginBottom: "40px",
+    "&.card-shadow": {
+      borderRadius: "10px",
+      "box-shadow": "0px 13px 26px rgb(109,184,241, 0.16)",
+
+      "& .img": {
+        maxWidth: "50px",
+        maxHeight: "50px",
       },
-      '& .title': {
-        color: '#2d3748',
-        fontWeight: '600',
+      "& .title": {
+        color: "#2d3748",
+        fontWeight: "600",
       },
-      '& .description': {
-        color:  '#718096',
-        fontWeight: '600',
-        maxWidth: '500px',
+      "& .description": {
+        color: "#718096",
+        fontWeight: "600",
+        maxWidth: "500px",
       },
-      '& .button-primary': {
-        background: ' #38d9a9',
+      "& .button-primary": {
+        background: " #38d9a9",
         border: 0,
-        borderRadius: '10px',
+        borderRadius: "10px",
         fontWeight: 600,
-        color: '#fff !important',
-        padding: '8px 20px !important',
-        '&:not([disabled]):hover': {
-          background: '#0ca678',
+        color: "#fff !important",
+        padding: "8px 20px !important",
+        "&:not([disabled]):hover": {
+          background: "#0ca678",
         },
       },
-      '& .boxs' : {
-          display: 'flex',
-          flexFlow: 'row wrap',
-          justifyContent: 'center',
-          width: '80%',
-          margin: 'auto',
-            '& .img' : {
-              maxWidth : '20px',
-              maxHeight : '20px'
-            },
-            '& .title': {
-              color: '#2d3748',
-              fontWeight: '600',
-            },
-            '& .description': {
-              color:  '#718096',
-              fontWeight: '600',
-              maxWidth: '500px',
-            },
-          },
-      
-        
-      },},
-      
-      '& .form-link': {
-        '& a': {
-          color: colors['text-gray-700'],
-          '& span': {
-            fontWeight: 500,
-          },
+      "& .boxs": {
+        display: "flex",
+        flexFlow: "row wrap",
+        justifyContent: "center",
+        width: "80%",
+        margin: "auto",
+        "& .img": {
+          maxWidth: "20px",
+          maxHeight: "20px",
+        },
+        "& .title": {
+          color: "#2d3748",
+          fontWeight: "600",
+        },
+        "& .description": {
+          color: "#718096",
+          fontWeight: "600",
+          maxWidth: "500px",
         },
       },
     },
-  );
+  },
 
-  export default useStyles ;
+  "& .form-link": {
+    "& a": {
+      color: colors["text-gray-700"],
+      "& span": {
+        fontWeight: 500,
+      },
+    },
+  },
+});
+
+export default useStyles;
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
+=======
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+>>>>>>> 8e07dc82 (added programme)
 
 const app = express();
 
@@ -13,6 +19,7 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // Routes
+<<<<<<< HEAD
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/request', require('./routes/request'));
@@ -24,6 +31,19 @@ if (process.env.NODE_ENV === 'production') {
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+=======
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/request", require("./routes/request"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+>>>>>>> 8e07dc82 (added programme)
   );
 }
 

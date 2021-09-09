2399006916671d2687e2ddd6e02b5fb78afe6329
Ad019El <a.dafi@esi-sba.dz>
2021-09-09 15:22:22 +0100

    edited request modal

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,22 +11,6 @@ import Alert from "./layout/Alert";
 import Footer from "./layout/Footer/Footer";
 
 // pages
-<<<<<<< HEAD
-import Login from './pages/Auth/Login';
-import Register from './pages/Auth/Register';
-import Profile from './pages/Dropdown/Profile';
-import Settings from './pages/Dropdown/Settings';
-import NotFound from './pages/NotFound'
-import Home from './pages/Home/Home';
-import Offers from './pages/Offers/Offers';
-import Request from './pages/Requests/Request';
-import Hea from './pages/Offers/description/Hea';
-import Soc from './pages/Offers/description/Soc';
-import Sol from './pages/Offers/description/Sol';
-import Oth from './pages/Offers/description/Oth';
-import Pending from './pages/Dropdown/Pending/pending';
-import Budget from './pages/Dropdown/Budget/budget';
-=======
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import Profile from "./pages/Dropdown/Profile";
@@ -41,7 +25,6 @@ import Sol from "./pages/Offers/description/Sol";
 import Oth from "./pages/Offers/description/Oth";
 import Requests from "./pages/Dropdown/requests";
 import Programme from "./pages/Programme/Programme";
->>>>>>> 715d48afeb8576c98abf4507f7c8dd5defcf1fab
 // Routes
 import PrivateRoute from "./routes/PrivateRoute";
 import AdminRoute from "./routes/AdminRoute";
@@ -50,30 +33,6 @@ function App() {
   return (
     <Router>
       <Provider store={store}>
-<<<<<<< HEAD
-        <div className='app'>            
-                <Route  path='/' component={Navbar} />
-              <Alert />
-              <Switch>
-                <Route exact path='/login' component={Login} />
-                <Route exact path='/register' component={Register} />
-                <PrivateRoute exact path='/u/:username' component={Profile} />
-                <PrivateRoute exact path='/settings' component={Settings} />
-                <PrivateRoute exact  path='/request' component={Request} />
-                <AdminRoute exact path='/pending' component={Pending}  />
-                <AdminRoute exact path='/statistics' component={Pending}  />
-                <AdminRoute exact path='/budget' component={Budget}  />
-                <Route exact path='/' component={Home} />
-                <Route exact path='/offers' component={Offers} />
-                <Route exact path='/offers/hea' component={Hea} />
-                <Route exact path='/offers/soc' component={Soc} />
-                <Route exact path='/offers/sol' component={Sol} />
-                <Route exact path='/offers/oth' component={Oth} />
-                <Route exact path='/404' component={NotFound} />
-                <Route component={NotFound} />
-              </Switch>
-              <Route path='/' component={Footer} />
-=======
         <div className="app">
           <Route path="/" component={Navbar} />
           <Alert />
@@ -97,7 +56,6 @@ function App() {
             <Route component={NotFound} />
           </Switch>
           <Route path="/" component={Footer} />
->>>>>>> 715d48afeb8576c98abf4507f7c8dd5defcf1fab
         </div>
       </Provider>
     </Router>
